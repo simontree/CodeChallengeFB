a9c9c818c0a0961a13f05b1d3daef451465859a4
simontree <simonroof@tutanota.com>
2022-06-10 13:10:38 +0200

    working on undefined-error

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -8,29 +8,64 @@ const Question = () => {
 
     const [questionIndex, setQuestionIndex] = useState(0);
     const [inputOptions, setInputOptions] = useState([]);
+    const [nextQuestionIDs, setNextQuestionIDs] = useState([]);
+    const [nextQuestionID, setNextQuestionID] = useState("");
+    const [questionNumber, setQuestionNumber] = useState(0);
     let navigate = useNavigate();
 
+    console.log(QuestionData.results.length-1)
+
     useEffect(() => {
-        const currentQuestion = QuestionData.results[questionIndex];
-        let answerOptions = [...currentQuestion.answers]
-        setInputOptions(answerOptions);
-    }, [questionIndex]);
+        if(questionIndex === QuestionData.results.length-1){
+            navigate('/finalpage');
+        }else{
+            try{
+                var currentQuestion = QuestionData.results[questionIndex];
+                var nextQuestionOptions = [...currentQuestion.next_question_id];
+                let answerOptions = [...currentQuestion.answers];
+                const questionNr = currentQuestion.question_number;
+                setInputOptions(answerOptions);
+                setNextQuestionIDs(nextQuestionOptions);
+                setQuestionNumber(questionNr);
+            }catch(error){
+                console.log(error);
+            }
+        }
+    }, [questionIndex, nextQuestionID, navigate]);
 
     const handleClick = (e) => {
-        const currentQuestion = QuestionData.results[questionIndex];
-        if(currentQuestion.next_question_id === ""){    //last question of tree
-            navigate('/finalpage');
-        }
+        const selectedAnswer = e.target.textContent;
+        const answerIndex = inputOptions.indexOf(selectedAnswer);
+        const nextQuestionIdOfClick = nextQuestionIDs[answerIndex];
+        setNextQuestionID(nextQuestionIdOfClick);
         
+        console.log(selectedAnswer)
+        console.log("indexOfSelectedAnswer: "+inputOptions.indexOf(selectedAnswer))
+        console.log("nextQuestionIdOfClick: "+nextQuestionIdOfClick);
+
+        console.log("typeof: "+typeof nextQuestionID);
+        console.log("nextQuestionID: "+nextQuestionID);
+    
+        // console.log("nextQuestionID.length: "+nextQuestionID.length);
+
+        if(nextQuestionID === 0){    //last question
+            navigate('/finalpage');
+        }else{
+            setQuestionIndex(nextQuestionIdOfClick-1)
+        }
     }
 
     return ( 
         <Box>
-            <Typography variant="h4">Question {questionIndex+1}</Typography>
-            <Typography variant="h5" mt={5}>{QuestionData.results[questionIndex].question}</Typography>
+            <Typography variant="h4">Question {questionNumber}</Typography>
+            <Typography variant="h5" mt={5}>{
+            (typeof QuestionData.results[questionIndex] !== 'undefined')?
+            QuestionData.results[questionIndex].question :
+            console.log("error")
+            }</Typography>
             {inputOptions.map((data, id) =>(
                 <Box key={id} mt={2}>
-                    <Button variant="contained">{data}</Button>
+                    <Button onClick={handleClick} variant="contained">{data}</Button>
                 </Box>
             ))}
         </Box>
