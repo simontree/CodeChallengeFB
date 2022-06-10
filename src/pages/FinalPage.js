50a93dea7abbb40008df71653395d13aaa84b5e8
simontree <simonroof@tutanota.com>
2022-06-10 21:25:42 +0200

    minor style changes

--- src/pages/FinalPage.js
+++ src/pages/FinalPage.js
@@ -13,13 +13,13 @@ const FinalPage = ({resultArray}) => {
     return ( 
         <Box>
             <Typography variant="h4" fontWeight="bold"> Final Page</Typography>
-            <Typography mt={10}><b>Your Questions and answers</b><br/>
+            <div style={{ 'margin-top' : '30px', 'fontWeight' : 'bold', 'fontSize' : '20px'}}>Your Questions and answers<br/>
             {resultArray.map((data, question) =>(
                 <Box key={question} mt={3}>
                     {data}
                 </Box>
             ))}
-            </Typography>
+            </div>
             <Box  mt={5}>
             <Button variant="contained" onClick={handleBackToLandingPage}>Back to the Start</Button>
             </Box>
