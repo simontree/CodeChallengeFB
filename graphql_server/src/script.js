f6c6076a3c4ee27380b849d58d2c7091fd58da9e
simontree <simonroof@tutanota.com>
2022-06-22 10:25:01 +0200

    linklist example graphql apollo works

--- graphql_server/src/script.js
+++ graphql_server/src/script.js
@@ -2,10 +2,11 @@ const { PrismaClient } = require("@prisma/client")
 
 const prisma = new PrismaClient()
 
-async function main() {
-  const newLink = await prisma.link.create({data: {
-    description: 'Fullstack tutorial for GraphQL',
-    url: 'www.howtographql.com',
+async function main() { 
+  const newLink = await prisma.link.create({
+    data: {
+      description: 'Fullstack tutorial for GraphQL',
+      url: 'www.howtographql.com',
   },})
   const allLinks = await prisma.link.findMany()
   console.log(allLinks)
